058cc17478264def2d3b3dcdbbbca39b0ec5fcc7
Juan Daniel <juandanielarce398@gmail.com>
2023-08-13 18:40:57 -0500

    Debounce search en la navbar

--- src/app/Modules/game-scrapy/shared/components/navbar/navbar.component.ts
+++ src/app/Modules/game-scrapy/shared/components/navbar/navbar.component.ts
@@ -1,13 +1,16 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Subject, debounceTime, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
   public search!: FormControl;
+
+  subjet:Subject<void> = new Subject<void>();
 
   @Output()
   event: EventEmitter<string> = new EventEmitter<string>();
@@ -15,10 +18,16 @@ export class NavbarComponent implements OnInit {
   constructor() {
     this.search = new FormControl('');
   }
+  ngOnDestroy(): void {
+    this.subjet.next();
+    this.subjet.complete();
+  }
 
   ngOnInit(): void {
-    this.search.valueChanges.subscribe(
-      (value: string) => value && this.event.emit(value)
-    );
+    this.search.valueChanges
+      .pipe(
+       takeUntil(this.subjet), 
+        debounceTime(500))
+      .subscribe((value: string) => value && this.event.emit(value));
   }
 }
